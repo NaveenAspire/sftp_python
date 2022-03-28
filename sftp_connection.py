70ba3d1c7eed447d7c86ddfbe9bd4a5b70bc654e
NaveenAspire <naveenkumar.mani@aspiresys.com>
2022-03-28 18:43:00 +0530

    Updated 28-03-2022

--- sftp_connection.py
+++ sftp_connection.py
@@ -1,15 +1,21 @@
 """This module that is used for retrive the file from sftp server and upload it into s3 bucket"""
 import pysftp
 import os
+import configparser
+import argparse
 from s3 import S3Service
 
+config = configparser.ConfigParser()
+config.read('sftp_config.ini')
 
 class SftpCon:
     """This is the class that contains methods for get file from sftp and upload to s3 """
-    def __init__(self) -> None:
+    def __init__(self,bucket_name) -> None:
         """This is the init method of the class of SftpCon"""
-        self.conn = pysftp.Connection(host='127.0.0.1',username='naveen', password='123')
-        self.bucket_name = 'aspire-sftp-python'
+        self.conn = pysftp.Connection(host=config['SFTP']['host'] ,username=config['SFTP']['username'],
+                                      password=config['SFTP']['password'])
+        self.bucket_name = bucket_name
+        print(self.conn)
         
     def get_sftp_file(self):
         """This method is used for download file from sftp server"""
@@ -21,7 +27,7 @@ class SftpCon:
         lpath = data_path+file_name
         self.conn.get(rpath,lpath)
         self.conn.close()   
-        self.upload_file_to_s3(lpath,self.bucket_name,'source/'+file_name)
+        # self.upload_file_to_s3(lpath,self.bucket_name,'source/'+file_name)
         
     def upload_file_to_s3(self,file_path,bucket_name,key):
         """This method that is used for upload the file into the s3 bucket."""
@@ -29,7 +35,12 @@ class SftpCon:
         s3.upload_file_to_s3(file_path,bucket_name,key) 
 
 def main():
-    sftp_con = SftpCon()
+    parser = argparse.ArgumentParser("For giving bucket name to store files fromsftp server")
+    parser.add_argument('--bucket_name', type=str,
+                        help='Enter th bucket name for store files retrived from sftp server', required=True)
+    args = parser.parse_args()
+    sftp_con = SftpCon(args.bucket_name)
+    print(args.bucket_name)
     sftp_con.get_sftp_file()
     
 if __name__ == "__main__":
